a3a438e75045aab9261c7d0f6da27c331659b45f
Bryson Elliott <brysonelliott@Brysons-MacBook-Pro.local>
2021-09-10 15:29:07 -0600

    Finished with SQL and Python

--- process.py
+++ process.py
@@ -1,12 +1,16 @@
+# This will open the text file allowing 
+# it to be used later on with the following function
 log_file = open("um-server-01.txt")
 
-
+# To change the day that it will listen for 
+# all you have to change is 'Tue' to 'Mon'
 def sales_reports(log_file):
     for line in log_file:
         line = line.rstrip()
         day = line[0:3]
-        if day == "Tue":
+        if day == "Mon":
             print(line)
 
+# It works.
 
 sales_reports(log_file)
---
+++ sql-skills.sql
@@ -0,0 +1,76 @@
+--Artist Section
+insert into artist (
+  name
+) 
+values (
+  'NF'
+), (
+  'Eminem'
+), (
+  'Brendan James'
+)
+
+select * from artist
+order by name limit 5;
+
+-- Extra Credit
+select * from artist
+order by name desc limit 10;
+
+select * from artist
+where name like 'Black%';
+
+select * from artist
+where name like '%Black%';
+
+--Employee Section
+select first_name, last_name, city from employee
+where city = 'Calgary';
+
+
+select employee_id from employee
+where first_name = 'Nancy' and last_name = 'Edwards';
+
+select * from employee
+where reports_to = 2;
+
+
+select count(*) from employee
+where city = 'Lethbridge';
+
+
+--Invoice Section
+select count(*) from invoice
+where billing_country = 'USA';
+
+select max(total) from invoice;
+
+select min(total) from invoice;
+
+select total from invoice
+where total >= 5;
+
+select count(*) from invoice
+where total <= 5;
+
+select sum(total) from invoice;
+
+
+--Join section
+select *
+from invoice i
+join invoice_line il on il.invoice_id = i.invoice_id
+where il.unit_price > 0.99;
+
+select i.invoice_date, c.first_name, c.last_name, i.total
+from invoice i
+join customer c on i.customer_id = c.customer_id;
+
+select c.first_name, c.last_name, e.first_name, e.last_name
+from customer c
+join employee e on c.support_rep_id = e.employee_id;
+
+select alb.title, art.name
+from album alb
+join artist art on alb.artist_id = art.artist_id;
+
